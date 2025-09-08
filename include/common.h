fb37bfa550e9b8070e71b3f156ba921a9c7b9c13
Enes Emre Aydogan <enes.e.aydogan@gmail.com>
2025-09-08 18:45:25 +0300

    feat: add error, info and logging macros

--- include/common.h
+++ include/common.h
@@ -6,15 +6,30 @@
 #define DOTMAN_VERSION "v0.1.0"
 
 typedef enum {
-    SCP_NONE = 0,
-    SCP_DOT,
-    SCP_DEP,
-    SCP_UNKNOWN,
+  SCP_NONE = 0,
+  SCP_DOT,
+  SCP_DEP,
+  SCP_UNKNOWN,
 } scp_type_t;
-
 
 // Vectors
 DEFINE_STR_VECTOR
 
+// Logging macros
+#define COLOR_RESET "\x1b[0m"
+#define COLOR_RED "\x1b[31m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_BLUE "\x1b[34m"
 
-#endif // !COMMON_H
+#define LOG_ERROR(msg, ...)                                                                \
+  fprintf(stderr, COLOR_RED "[ERROR] %s:%d:%s()\n" COLOR_RESET "   ↳ " msg "\n", __FILE__, \
+          __LINE__, __func__, ##__VA_ARGS__)
+
+#define LOG_WARN(msg, ...)                                                                   \
+  fprintf(stderr, COLOR_YELLOW "[WARN] %s:%d:%s()\n" COLOR_RESET "   ↳ " msg "\n", __FILE__, \
+          __LINE__, __func__, ##__VA_ARGS__)
+
+#define LOG_INFO(msg, ...)                                                                 \
+  fprintf(stdout, COLOR_BLUE "[INFO] %s:%d:%s()\n" COLOR_RESET "   ↳ " msg "\n", __FILE__, \
+          __LINE__, __func__, ##__VA_ARGS__)
+#endif  // !COMMON_H
